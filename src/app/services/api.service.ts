3c80add34f4750caa7325a350437a6777f8dc9ce
Hector G Fuentes Gaete <ext_hgfuentes@falabella.cl>
2021-11-21 00:57:42 -0300

    fix conf env y proxy prod

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,6 @@ export class ApiService {
   ) { }
 
   getNoticias() {
-    return this.http.get('/api/noticias').toPromise();
+    return this.http.get(`${environment.apiUrl}/api/noticias`).toPromise();
   }
 }
